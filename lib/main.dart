4cfd9f089b01c3fb772d52a708b62c6695d9ca76
Leonid Gusev <rastrast207@gmail.com>
2024-06-02 16:03:16 +0300

    remove env

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,12 @@ import '/ui/core/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
 
   runApp(const App());
 }
